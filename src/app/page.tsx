0648d033066e426184e9681a0b53dc0d4bbe9301
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-16 22:50:27 +0530

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-"use client";
 
-import { useRouter } from "next/navigation";
-import { useContext, useEffect, useState } from "react";
-import { Context } from "../components/context/context";
 import FooterNav from "@/components/layout/FooterNav";
 import AuthProtector from "@/components/layout/AuthProtector";
+import Header from "@/components/layout/Header";
+import StoriesShowCase from "@/_components/StoriesShowCase";
 
 export default function ProtectedPage() {
   return (
     <AuthProtector>
+      <Header />
+      <div className="mt-23 max-w-[1280px] px-6 no-scrollbar overflow-x-scroll mx-auto">
+        <StoriesShowCase />
+        sdf
+      </div>
+
       <FooterNav />
     </AuthProtector>
   );
