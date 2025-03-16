d4b7f7423d19db9815937c26e1df567989e1f788
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-16 22:50:27 +0530

    Add SingleStory component

---
+++ src/components/ui/SingleStory.tsx
@@ -0,0 +1,11 @@
+export default function SingleStory() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="p-[2px] bg-gradient-to-r from-[#feda75] via-[#fa7e1e] to-[#d62976] rounded-full">
+        <div className="min-h-[64px] min-w-[64px]  rounded-full bg-white flex items-center justify-center border-[3px] border-black"></div>
+      </div>
+
+      <p className="max-w-[76px] truncate">Namesdfsffffffff</p>
+    </div>
+  );
+}
