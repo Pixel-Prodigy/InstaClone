82b0978075614d1e2c49acce07ecc2ef8794bdfa
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-16 22:50:27 +0530

    Update context provider

--- src/components/context/contextProvider.tsx
+++ src/components/context/contextProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { Context } from "./context";
-import { useRouter } from "next/navigation";
 
 export default function ContextProvider({
   children,
@@ -9,19 +8,60 @@ export default function ContextProvider({
   children: React.ReactNode;
 }) {
   const [user, setUser] = useState<User | null>(null);
-  const router = useRouter();
+  const [token, setToken] = useState<string | null>(null);
 
-  const login = (user: User | null) => {
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+
+    if (!storedToken) {
+      logout();
+      return;
+    }
+
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/me`, {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          console.error("Failed to fetch user data");
+          logout();
+          return;
+        }
+
+        const userData = await response.json();
+        setUser(userData);
+        setToken(storedToken);
+      } catch (error) {
+        console.error("Error sending token:", error);
+        logout();
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const login = (user: User | null, token: string) => {
     setUser(user);
+    setToken(token);
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
   };
 
   const logout = () => {
     setUser(null);
+    setToken(null);
+    localStorage.removeItem("user");
     localStorage.removeItem("token");
   };
 
   return (
-    <Context.Provider value={{ login, logout, user }}>
+    <Context.Provider value={{ login, logout, user, token }}>
       {children}
     </Context.Provider>
   );
