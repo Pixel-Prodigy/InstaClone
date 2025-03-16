fe6d626a001aa1837ae9dfd7cb46be1d230c7b98
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-16 22:50:27 +0530

    Update protected layout

--- src/app/(Protectd_Pages)/layout.tsx
+++ src/app/(Protectd_Pages)/layout.tsx
@@ -3,10 +3,8 @@ import FooterNav from "@/components/layout/FooterNav";
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProtector>
       <div className="relative">
         {children} <FooterNav />
       </div>
-    </AuthProtector>
   );
 }
