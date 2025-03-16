c52f90378fba21e884fac654d6ea585539dd5025
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-16 22:50:27 +0530

    Update AuthProtector layout

--- src/components/layout/AuthProtector.tsx
+++ src/components/layout/AuthProtector.tsx
@@ -6,43 +6,16 @@ import { Context } from "../context/context";
 
 const AuthProtector = ({ children }: { children: React.ReactNode }) => {
   const ctx = useContext(Context);
-  if (!ctx) return null;
-  const { user, login } = ctx;
   const router = useRouter();
 
+  if (!ctx) return null;
+  const { user } = ctx;
+
   useEffect(() => {
-    const userCheck = async () => {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        login(null);
-        router.push("/login");
-        return;
-      }
-
-      try {
-        const res = await fetch("http://localhost:5000/me", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const data = await res.json();
-        if (res.ok && data.success) {
-          login(data.data);
-        } else {
-          login(null);
-          router.push("/login");
-        }
-      } catch (err) {
-        console.log(err);
-        login(null);
-        router.push("/login");
-      }
-    };
-
-    userCheck();
-  }, [router]);
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user, router]);
 
   return user ? <>{children}</> : null;
 };
