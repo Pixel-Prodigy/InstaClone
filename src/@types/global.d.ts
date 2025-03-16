f2596d624d9a1c9bf5c5826e5494f78195d959bf
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-16 22:50:27 +0530

    Update global types

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -5,19 +5,28 @@ declare global {
     email?: string;
     password?: string;
     token?: string;
-    name?: string;
+    username?: string;
     avatar?: string;
   };
   type Context = {
     user: User | null;
     setUser?: Dispatch<SetStateAction<User | null>>;
-    login: (user: User | null) => void;
+    login: (user: User | null, token: string) => void;
     logout: () => void;
+    token: string | null
   };
   type AuthApiResponse = {
     success: boolean;
     message?: string;
     data?: User;
     token?: string;
+  };
+  type PostRequest = {
+    content?: string;
+    hideComments: boolean;
+    hideLikes: boolean;
+    userId: string;
+    userId: string;
+    imageUrl: string;
   };
 }
