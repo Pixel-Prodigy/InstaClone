4ab49db639fe738005f2f660fbbaa80ddd05f5c9
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-20 00:04:02 +0530

    Added/Updated server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,16 @@ import { signup } from "./auth/signup.js";
 import { login } from "./auth/login.js";
 import { newPost } from "./posts/newPost.js";
 import { authenticateUser } from "./auth/authenticationChecker.js";
+import { editProfile } from "./profile/editProfile.js";
+import {allPosts} from "./posts/allPosts.js";
+import {addLiking} from "./likings/addLiking.js";
+
 import express from "express";
 import cookieParser from "cookie-parser";
-
 const app = express();
 const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 const PORT = serverUrl.split(":")[2];
+
 app.use(
   cors({
     origin: process.env.NEXT_PUBLIC_FRONTEND_URL,
@@ -16,16 +20,17 @@ app.use(
     credentials: true,
   })
 );
-console.log()
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.post("/signup", signup);
-
 app.post("/login", login);
 app.get("/me", authenticateUser);
+app.get("/allPosts" , allPosts)
 app.post("/newPost", newPost);
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.post("/addLiking", addLiking);
+app.put("/editProfile", editProfile);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on ${serverUrl}`);
 });
