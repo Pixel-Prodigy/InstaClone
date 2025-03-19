4134037ed684eebf5791eb7625451131275ef4bc
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-20 00:04:02 +0530

    Added/Updated posts/newPost.js

--- src/server/posts/newPost.js
+++ src/server/posts/newPost.js
@@ -1,16 +1,19 @@
-import { supabase } from "../supabaseClient.js";
+import { supabase } from "../clients/supabaseClient.js";
 import { randomUUID } from "crypto";
 import { prisma } from "../clients/prisma.js";
+import jwt from 'jsonwebtoken'
 export async function newPost(req, res) {
   try {
-    const { content, imageUrl, hideComments, hideLikes, user } = req.body;
-    console.log(user);
+    const { content, imageUrl, hideComments, hideLikes } = req.body;
+    const token= req.cookies.authToken;
+    
     if (!imageUrl) {
       return res
         .status(400)
         .json({ success: false, message: "No image provided" });
     }
-
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+console.log(user)
     const base64Data = imageUrl.replace(/^data:image\/\w+;base64,/, "");
     const fileBuffer = Buffer.from(base64Data, "base64");
     const fileName = `post-${Date.now()}-${randomUUID()}.png`;
@@ -20,13 +23,14 @@ export async function newPost(req, res) {
       .upload(fileName, fileBuffer, { contentType: "image/png" });
 
     if (error) {
-      return res.status(400).json({ success: false, message: error.message });
+      return res.status(400).json({
+        success: false,
+        message: `Error while uploading to storage: ${error.message}`,
+      });
     }
 
-    const publicUrl = supabase.storage
-      .from("postimages")
-      .getPublicUrl(fileName);
-
+    const publicUrl = supabase.storage.from("postimages").getPublicUrl(fileName)
+      .data.publicUrl;
     await prisma.post.create({
       data: {
         userId: user.id,
@@ -39,10 +43,9 @@ export async function newPost(req, res) {
     return res.status(200).json({
       success: true,
       message: "Post created successfully",
-      imageUrl: publicUrl,
       data,
     });
   } catch (err) {
-    return res.status(500).json({ success: false, message: err.message });
+    return res.status(500).json({ success: false, message: `catched error in newPost.js:${err.message}` });
   }
 }
