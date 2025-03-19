41b25f80dcb74e326428f18ed5903e59d5a06b93
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-20 00:04:02 +0530

    Added/Updated posts/allPosts.js

---
+++ src/server/posts/allPosts.js
@@ -0,0 +1,33 @@
+import { prisma } from "../clients/prisma.js";
+
+export async function allPosts(req, res) {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        user: true,
+        comments: true,
+        likes: true
+      },
+    });
+
+    const filteredPosts = posts
+      .filter((post) => !post.user.isPrivate) 
+      .map((post) => ({
+        ...post,
+        imageUrl: post.imageUrl,
+        user: post.user,
+        content: post.content,
+        hideComments: post.hideComments,
+        hideLikes: post.hideLikes,
+      }));
+
+    return res.status(200).json({
+      success: true,
+      data: filteredPosts,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ success: false, message: `Caught error in allPosts: ${err}` });
+  }
+}
