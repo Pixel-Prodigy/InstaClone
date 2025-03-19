dd58fbca0fe00694aeebfe6e4c4af371990fff33
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-20 00:04:02 +0530

    Added/Updated auth/authenticationChecker.js

--- src/server/auth/authenticationChecker.js
+++ src/server/auth/authenticationChecker.js
@@ -2,18 +2,25 @@ import { prisma } from "../clients/prisma.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+
 export const authenticateUser = async (req, res) => {
   try {
     const token = req.cookies.authToken;
-    console.log(req.cookies);
+
     if (!token) {
-      return res.status(401).json({ success: false, message: "Unauthorized" });
+      return res.status(201).json({ success: false, message: "Unauthorized" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
     const user = await prisma.user.findUnique({
       where: { id: decoded.id },
+      include: {
+        followers: true,
+        following: true,
+        posts: true,
+        likes: true,
+        comments: true,
+      },
     });
 
     if (!user) {
@@ -23,9 +30,18 @@ export const authenticateUser = async (req, res) => {
     res.status(200).json({
       success: true,
       data: {
+        id: user.id,
         email: user.email,
         username: user.username,
-        avatar: user.avatar,
+        avatar: user.avatar || "/user.jpg",
+        followers: user.followers,
+        following: user.following,
+        posts: user.posts,
+        likes: user.likes,
+        comments: user.comments,
+        socialLinks: user.socialLinks || [],
+        isPrivate: user.isPrivate,
+        bio: user.bio,
       },
     });
   } catch (err) {
