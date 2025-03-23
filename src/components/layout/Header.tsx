2fecdf849543b5ca5c870438c066072e91553d82
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-23 09:46:00 +0530

    Update Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,10 +2,10 @@ import { FaCamera, FaTv, FaRegPaperPlane } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <div className="flex fixed top-0 left-0 mb-80 px-10 w-full items-end h-[88px] justify-center pb-[14px] bg-[#101010] border-b-[1px]  border-b-gray-500/20">
+    <div className="flex fixed top-0 left-0 mb-80 px-10 w-full items-end h-[78px] justify-center pb-[14px] bg-[#101010] border-b-[1px]  border-b-gray-500/20">
       <div className="flex  items-center max-w-[1280px] justify-between  w-full h-fit ">
         <FaCamera className="w-[28px]  h-[24px] text-white cursor-pointer fill-none stroke-white stroke-20" />
-        <div className="overflow-hidden  max-h-[36px] ">
+        <div className="overflow-hidden ml-10  max-h-[36px] ">
           <img
             src="/insta.png
             "
