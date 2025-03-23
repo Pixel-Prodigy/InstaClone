7b27aa37855f3be4bc6481bca0e5dc8e88a4b539
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-23 09:46:00 +0530

    Update StoriesShowCase.tsx

--- src/_components/StoriesShowCase.tsx
+++ src/_components/StoriesShowCase.tsx
@@ -2,7 +2,7 @@ import SingleStory from "../components/ui/SingleStory";
 
 export default function StoriesShowCase() {
   return (
-    <div className="flex items-center gap-4 no-scrollbar overflow-x-scroll max-w-[100vw] h-fit ">
+    <div className="flex items-center ml-3 gap-4 no-scrollbar overflow-x-scroll max-w-[100vw] h-fit ">
       {Array.from({ length: 100 }).map((_, i) => (
         <SingleStory key={i} />
       ))}
