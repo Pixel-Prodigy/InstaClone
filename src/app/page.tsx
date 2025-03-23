d04582daea4255d131c28c61e891804ecdd2ec6c
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-03-23 09:46:00 +0530

    Update main app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
-
 import FooterNav from "@/components/layout/FooterNav";
 import AuthProtector from "@/components/layout/AuthProtector";
 import Header from "@/components/layout/Header";
 import StoriesShowCase from "@/_components/StoriesShowCase";
+import { AllPosts } from "@/_components/AllPosts";
 
 export default function ProtectedPage() {
   return (
     <AuthProtector>
-      <Header />
-      <div className="mt-23 max-w-[1280px] px-6 no-scrollbar overflow-x-scroll mx-auto">
-        <StoriesShowCase />
-        sdf
-      </div>
+      <div className="relative max-w-[1280px] mx-auto">
+        <Header />
 
-      <FooterNav />
+        <div className="flex max-w-[520px] mx-auto  flex-col mt-26 gap-4 "> 
+          <div className="max-w-[100vw] h-[100px] overflow-x-auto no-scrollbar">
+            <StoriesShowCase />
+          </div>
+
+          <div className="flex-1 pb-40   ">
+            <AllPosts />
+          </div>
+        </div>
+
+        <FooterNav />
+      </div>
     </AuthProtector>
   );
 }
